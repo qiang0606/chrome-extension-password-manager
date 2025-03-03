110e72907fa9b2643d96ae94ee1794492d007849
strongsunday.zq <qiang.zhao1>
2025-03-03 10:52:27 +0800

    build: build

--- dist/background.js
+++
@@ -1,1 +0,0 @@
-chrome.runtime.onInstalled.addListener(()=>{console.log("Password Manager extension installed.")});
--- vite.config.js
+++ vite.config.js
@@ -1,58 +1,24 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import copy from "rollup-plugin-copy";
 import { resolve } from "path";
 
-export default defineConfig(({ mode }) => {
-  const isWeb = mode === "web";
-
-  const config = {
-    plugins: [
-      vue(),
-      copy({
-        targets: [
-          { src: "src/manifest.json", dest: "dist" },
-          { src: "src/icons", dest: "dist" },
-          {
-            src: isWeb ? null : "index.html",
-            dest: "dist",
-            transform: (contents) =>
-              contents.toString().replace("/src/main.js", "popup.js"),
-            rename: "popup.html",
-          },
-        ].filter(Boolean),
-        hook: "writeBundle",
-        verbose: true,
-      }),
-    ],
-    build: {
-      // minify: false,
-      outDir: "dist",
-      emptyOutDir: true,
-      rollupOptions: {
-        input: {
-          ...(isWeb ? { app: resolve(__dirname, "index.html") } : {}),
-          popup: resolve(__dirname, "src/main.js"),
-          content: resolve(__dirname, "src/content.js"),
-          background: resolve(__dirname, "src/background.js"),
-        },
-        output: {
-          entryFileNames: "[name].js",
-          chunkFileNames: "[name].[hash].js",
-          assetFileNames: "[name].[ext]",
-        },
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
       },
     },
-    server: {
-      port: 4173,
-      open: true,
+    outDir: "dist",
+    assetsDir: "assets",
+    copyPublicDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "/icons": resolve(__dirname, "public/icons"),
     },
-    resolve: {
-      alias: {
-        "@": resolve(__dirname, "src"),
-      },
-    },
-  };
-
-  return config;
+  },
+  publicDir: "public",
 });
